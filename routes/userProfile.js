9fd2acb04fc4385aed2f7babf3e6d75c44166711
Polowis <tranphuochung0912@gmail.com>
2019-05-16 19:12:39 +1000

    fixed routing

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -1,10 +1,10 @@
 const User = require('../models/users');
 const express = require('express');
 var router = express.Router();
+//var app = express();
 
-
-router.get('/profile', function(req, res) {
-    User.findById({_id:req.user._id}).then((user) =>{
+router.get('/', function(req, res) {
+    User.findById({_id:req.user}).then((user) =>{
         if(user){
             res.render('profile', {
                 title: 'User Profile',
